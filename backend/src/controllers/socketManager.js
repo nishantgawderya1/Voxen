003bd83729ebe4d64bad242f761f384e6caf8277
Nishant <nishantgawdiya@gmail.com>
2026-06-28 13:02:52 +0530

    backend complete , register , login , message , disconnect - Next starting with frontend thing

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -5,7 +5,14 @@ let timeOnline = {};
 let messages = {};
 
 const connectToSocket = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["*"],
+      credentials: true,
+    },
+  });
 
   io.on("connection", (socket) => {
     socket.on("join-call", (path) => {
